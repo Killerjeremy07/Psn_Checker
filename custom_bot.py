c1bade35289e1dda1ed8a306bce73e9a3a91d37b
Killerjeremy07 <162461698+Killerjeremy07@users.noreply.github.com>
2024-07-31 17:07:53 +0200

    Delete custom_bot.py

--- custom_bot.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-from psnawp_api import PSNAWP
-
-
-class Bot(commands.Bot):
-    def __init__(self, psn_api_token: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.psnawp = PSNAWP(psn_api_token)
